03a04be7e55fb5a3c641e0bf8d218c41b70703ee
oscarArrietojas <69063510+oscarArrietojas@users.noreply.github.com>
2020-08-15 23:08:12 -0600

    Update Persona.h

--- Persona.h
+++ Persona.h
@@ -1,21 +1,38 @@
-#pragma once
-#include<iostream>
-#include<sstream>
-#include"Carro.h"
+#ifndef LAB03_UML_RELATIONSHIPS_XRAY_PERSONA_H
+#define LAB03_UML_RELATIONSHIPS_XRAY_PERSONA_H
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
 using namespace std;
 
-class Persona
-{
+class Persona {
 private:
-	string nombre;
-	int edad;
+    string nombre;
+    string apellido;
+    int edad;
+    string licencia;
 public:
-	Persona();
-	Persona(string nombre, int edad);
-	~Persona();
-	int getNombre() const;
-	void setNombre(int nombre);
-	int getEdad() const;
-	void setEdad(int edad);
-	string toString;
+    Persona();
+    Persona(string nombre,string apellido, int edad,string licencia);
+    virtual ~Persona();
+
+    string getNombre();
+    void setNombre(string nombre);
+
+    string getApellido();
+    void setApellido(string apellido);
+
+    int getEdad();
+    void setEdad(int edad);
+
+    string getLicencia();
+    void setLicencia(string licencia);
+
+    string toString();
 };
+
+
+#endif //LAB03_UML_RELATIONSHIPS_XRAY_PERSONA_H
