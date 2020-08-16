2b39a2a3551a1ae4a9512f17f5d52b4cc0def852
oscarArrietojas <69063510+oscarArrietojas@users.noreply.github.com>
2020-08-15 23:12:18 -0600

    Update Carro.h

--- Carro.h
+++ Carro.h
@@ -1,20 +1,35 @@
-#pragma once
-#include<iostream>
-#include<sstream>
-#include"TrasporteTerrestre.h"
+#ifndef LAB03_UML_RELATIONSHIPS_XRAY_CARRO_H
+#define LAB03_UML_RELATIONSHIPS_XRAY_CARRO_H
+
+#include "TransporteTerrestre.h"
+#include "Persona.h"
+#include "Motor.h"
+#include <string>
+
 using namespace std;
 
-class Carro : public TrasporteTerrestre
-{
+class Carro: public TransporteTerrestre {
 private:
-	string tipo;
-	string modelo;
+    Motor* motor;
+    string placa;
+    Persona* duenno;
 public:
-	Carro();
-	~Carro();
-	string getTipo() const;
-	void setTipo();
-	string getModelo() const;
-	void setModelo();
-	string toString();
+    Carro();
+    Carro(string marca, int velocidadMaxima, int cantidadPasajeros, int ruedas,Motor* motor,
+          string placa, Persona* duenno);
+    virtual ~Carro();
+
+    Motor* getMotor();
+    void setMotor(Motor* motor);
+
+    string getPlaca();
+    void setPlaca(string placa);
+
+    Persona* getDuenno() ;
+    void setDuenno(Persona* duenno);
+
+    string toString();
 };
+
+
+#endif //LAB03_UML_RELATIONSHIPS_XRAY_CARRO_H
