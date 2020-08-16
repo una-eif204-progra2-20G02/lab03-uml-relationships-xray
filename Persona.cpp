5e873699aff20dd1dbdc0d1aee8cf862c969ebf3
oscarArrietojas <69063510+oscarArrietojas@users.noreply.github.com>
2020-08-15 23:10:19 -0600

    Update Persona.cpp

--- Persona.cpp
+++ Persona.cpp
@@ -1,16 +1,46 @@
-
 #include "Persona.h"
 
-Persona::Persona(){}
-Persona::Persona(string nombre, int edad) 
-{
+Persona::Persona() {}
 
+Persona::Persona(string nombre,string apellido, int edad,string licencia) : nombre(
+        nombre), apellido(apellido), edad(edad),licencia(licencia) {}
+
+Persona::~Persona() {}
+
+string Persona::getNombre() {
+    return nombre;
 }
-Persona::~Persona(){}
-int Persona:: getNombre() const{}
-void Persona:: setNombre(int nombre){}
-int Persona:: getEdad() const{}
-void Persona::setEdad(int edad){}
-string toString{
 
+void Persona::setNombre(string nombre) {
+    Persona::nombre = nombre;
+}
+
+string Persona::getApellido() {
+    return apellido;
+}
+
+void Persona::setApellido(string apellido) {
+    Persona::apellido = apellido;
+}
+
+int Persona::getEdad() {
+    return edad;
+}
+
+void Persona::setEdad(int edad) {
+    Persona::edad = edad;
+}
+
+
+string Persona::getLicencia() {
+    return licencia;
+}
+
+void Persona::setLicencia(string licencia) {
+    Persona::licencia = licencia;
+}
+
+string Persona::toString() {
+    return "Nombre: "+ nombre +" "+ apellido + "\n Edad: "+ to_string(edad) + "\n"
+    + "Licencia: " + licencia + "\n"; //Por mejorar
 }
